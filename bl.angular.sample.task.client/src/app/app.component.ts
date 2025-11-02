db25bfeb0d938ce11b3062fa75144c731e2de85d
Guilherme Bley <guilhermecbley@gmail.com>
2025-11-02 00:48:26 -0300

    - angular app

--- bl.angular.sample.task.client/src/app/app.component.ts
+++ bl.angular.sample.task.client/src/app/app.component.ts
@@ -1,38 +1,17 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import { AccountService } from './_services';
+import { User } from './_models';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
-})
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
+@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+export class AppComponent {
+  user?: User | null;
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe(x => this.user = x);
   }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+  logout() {
+    this.accountService.logout();
   }
-
-  title = 'bl.angular.sample.task.client';
 }
--- bl.angular.sample.task.client/src/app/app.module.ts
+++ bl.angular.sample.task.client/src/app/app.module.ts
@@ -1,19 +1,28 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
